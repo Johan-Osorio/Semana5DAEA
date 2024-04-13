423cf5ce94ad5e7a63273be07d90c6b927d7bec1
Alumno <Alumno@LAB1504-29>
2024-04-13 12:57:22 -0500

    Agregar archivos de proyecto.

---
+++ Semana5/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Semana5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Data Source=LAB1504-29\\SQLEXPRESS;Initial Catalog=Neptuno;User Id=johan;Password=123456";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string idCliente = "JECM";
+            string nombreCompañia = "IDK";
+            string nombreContacto = "Aylin";
+            string cargoContacto = "Traductora";
+            string direccion = "Huaycan";
+            string ciudad = "Lima";
+            string region = "Lima";
+            string codPostal = "15483";
+            string pais = "Peru";
+            string telefono = "999888777";
+            string fax = "Factos";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand("USP_InsertarClientes", connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@idCliente", idCliente);
+                    command.Parameters.AddWithValue("@NombreCompañia", nombreCompañia);
+                    command.Parameters.AddWithValue("@NombreContacto", nombreContacto);
+                    command.Parameters.AddWithValue("@CargoContacto", cargoContacto);
+                    command.Parameters.AddWithValue("@Direccion", direccion);
+                    command.Parameters.AddWithValue("@Ciudad", ciudad);
+                    command.Parameters.AddWithValue("@Region", region);
+                    command.Parameters.AddWithValue("@CodPostal", codPostal);
+                    command.Parameters.AddWithValue("@Pais", pais);
+                    command.Parameters.AddWithValue("@Telefono", telefono);
+                    command.Parameters.AddWithValue("@Fax", fax);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Cliente agregado correctamente.");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Actualizar cliente");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Borrar cliente");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = new SqlCommand("USP_ListarClientes", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+
+                    dgClientes.ItemsSource = dt.DefaultView;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al listar clientes: " + ex.Message);
+                }
+            }
+        }
+    }
+}
